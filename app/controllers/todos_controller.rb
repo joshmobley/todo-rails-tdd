f6ac46fa04be1ed58fd46186850cdbd7af4b3e19
Josh Mobley <josh@joshmobley.net>
2018-08-12 23:42:53 -0400

    refactoring session email into methods inside of application controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
     before_action :authenticate
 
     def index
-        @todos = Todo.where email: session[:current_email]
+        @todos = Todo.where email: current_email
     end
 
     def new
@@ -11,7 +11,7 @@ class TodosController < ApplicationController
     end
 
     def create
-        Todo.create todo_params.merge email: session[:current_email]
+        Todo.create todo_params.merge email: current_email
         redirect_to root_path
     end
 
